485ab4e3b100ab6cb7a08e1841b5f81494d67ef8
Robert Lockett <lockettr@hotmail.com>
2022-09-23 19:57:46 +0200

    testing automatic deply v2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test changes with automatic deploy
+          Test changes with automatic deploy ver2
         </p>
         <a
           className="App-link"
